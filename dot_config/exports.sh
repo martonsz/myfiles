1b87941f5210dbea16c42c8e5524dc40d2e7b13d
Marton Szucs <>
2024-04-02 19:33:14 +0000

    moving XDG_ variables to the top of .zshrc

--- dot_config/exports.sh
+++ dot_config/exports.sh
@@ -1,8 +1,3 @@
-export XDG_CONFIG_HOME="$HOME/.config"
-export XDG_DATA_HOME="$HOME/.local/share"
-export XDG_STATE_HOME="$HOME/.local/state"
-
-export PATH="$HOME/.local/bin:$HOME/bin:$PATH"
 if hash nvim 2> /dev/null; then
     export EDITOR=nvim
 elif hash vim 2> /dev/null; then
