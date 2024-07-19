6de15dde79a2aefb3270a896d82aa29b0b150199
Marton Szucs <2458236+martonsz@users.noreply.github.com>
2024-07-19 19:35:33 +0200

    starting ssh-agent

---
+++ dot_config/ssh-agent.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -x
+
+SSH_ENV=$HOME/.ssh/environment
+function start_agent {
+    ssh-agent | sed 's/^echo/#echo/' >"${SSH_ENV}"
+    chmod 600 "${SSH_ENV}"
+    # shellcheck disable=SC1090
+    source "${SSH_ENV}" >/dev/null
+}
+
+if [ -f "${SSH_ENV}" ]; then
+    # shellcheck disable=SC1090
+    source "${SSH_ENV}" >/dev/null
+    ps -efp "${SSH_AGENT_PID}" | grep -q ssh-agent$ || {
+        start_agent
+    }
+else
+    start_agent
+fi
