0c18e348c631f73027ef4c0df0a3d654f0ec8a08
Marton Szucs <2458236+martonsz@users.noreply.github.com>
2024-07-19 19:19:38 +0000

    checking if we are using ssh agent forward

--- dot_config/ssh-agent.sh
+++ dot_config/ssh-agent.sh
@@ -1,19 +1,22 @@
 #!/bin/bash -x
 
 SSH_ENV=$HOME/.ssh/environment
+
 function start_agent {
-    ssh-agent | sed 's/^echo/#echo/' >"${SSH_ENV}"
-    chmod 600 "${SSH_ENV}"
-    # shellcheck disable=SC1090
-    source "${SSH_ENV}" >/dev/null
+  ssh-agent | sed 's/^echo/#echo/' >"${SSH_ENV}"
+  chmod 600 "${SSH_ENV}"
+  # shellcheck disable=SC1090
+  source "${SSH_ENV}" >/dev/null
 }
 
-if [ -f "${SSH_ENV}" ]; then
+if [ -z "$SSH_CONNECTION" ] && [ -z "$SSH_CLIENT" ]; then
+  if [ -f "${SSH_ENV}" ]; then
     # shellcheck disable=SC1090
     source "${SSH_ENV}" >/dev/null
     ps -efp "${SSH_AGENT_PID}" | grep -q ssh-agent$ || {
         start_agent
     }
-else
+  else
     start_agent
+  fi
 fi
